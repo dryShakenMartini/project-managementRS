07758b650c878c25ffbb3b5274f4f41561c7ff4a
dryShakenMartini <radmilica90@gmail.com>
2016-03-07 23:29:03 +0100

    Added startup pages and initial controller

---
+++ src/ProjectManagementRS/Controllers/Web/ApplicationController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace ProjectManagementRS.Controllers.Web
+{
+    public class ApplicationController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Users()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        public IActionResult Time()
+        {
+            return View();
+        }
+    }
+}
