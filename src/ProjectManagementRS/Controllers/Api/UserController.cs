b50df8bcc2cba5d3a2c1a9a843df7a09d5b73d4a
dryShakenMartini <radmilica90@gmail.com>
2016-04-02 13:18:13 +0200

    Add Api controller to solution. Work in progress.

---
+++ src/ProjectManagementRS/Controllers/Api/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace ProjectManagementRS.Controllers.Api
+{
+    [Route("api/users")]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public JsonResult Get()
+        {
+            return Json(new { Name = "Rada" });
+        }
+
+    }
+}
