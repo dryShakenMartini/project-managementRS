d00e8e9e6d2a7ae4ebec031741bd4289a8a75f7f
dryShakenMartini <radmilasekulic90@gmail.com>
2016-06-10 23:12:41 +0200

    Change port.

--- src/ProjectManagementRS/wwwroot/js/usersController.js
+++ src/ProjectManagementRS/wwwroot/js/usersController.js
@@ -1,53 +1,27 @@
 ﻿"use strict";
 
-angular.module("app-darkstar").controller("rigel.UsersController", ["$scope","$http", "ModalDialogService", "$q",
-    function ($scope, $http, ModalDialogService, $q) {
+angular.module("app-darkstar").controller("darkstar.UsersController", ["$scope","$http",
+    function($scope, $http) {
         var vm = this;
         $scope.gridOptions = {
             enableColumnResizing: true,
         };
-
-        $scope.gridOptions.onRegisterApi = function (gridApi) {
-            $scope.myGridApi = gridApi;
-        };
-
-        $scope.data = {};
-
-        var baseUrl = $("base").first().attr("href");
-
-        $http({ method: "GET", url: "http://localhost:8000/api/users/columns" })
+        
+        $http({ method: "GET", url: "http://localhost:53213/api/users/columns" })
         .then(function (response) {
             $scope.gridOptions.columnDefs = response.data;
         }, function (response) {
             $scope.gridOptions.columnDefs = [];
         });
 
+        $http({ method: "GET", url: "http://localhost:53213/api/users" })
+        .then(function (response) {
+            $scope.gridOptions.data = response.data;
+        },function (response) {
+            $scope.gridOptions.data = [];
+        });
 
-        var getUsers = function () {
-            $http({ method: "GET", url: "http://localhost:8000/api/users" })
-            .then(function (response) {
-                $scope.gridOptions.data = response.data;
-            }, function (response) {
-                $scope.gridOptions.data = [];
-            });
-        };
-
-        vm.addUser = function () {
-            var promise;
-            ModalDialogService.showModal({
-                templateUrl: baseUrl + "Application/UserDialog",
-                controller: "ModalController"
-            }).then(function (modal) {
-              
-                modal.element.modal();
-                modal.close.then(function (result) {
-                    $http({ method: "POST", url: "http://localhost:8000/api/users", data: result });
-                    $scope.gridOptions.data = [];
-                    getUsers();
-                });
-            });
-        };
         vm.name = "Rada";
-        getUsers();
+
     }
 ]);
